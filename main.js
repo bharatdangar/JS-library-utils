9f95af25dad83122587740918de0da058e2de19b
Bharat Dangar <bharatdangar@gmail.com>
2022-01-07 14:07:33 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/*Add active class on navbar items when clicked 
+/*Add active class on navbar items when clicked For each
 ===========================================================*/
 const navbar = document.querySelector(".navbar");
 const navbarItems = navbar.querySelectorAll("li");
@@ -40,13 +40,83 @@ const preloadImages = (selector = 'img') => {
 
 
 // Preload Fonts using WebFont Loader
+//https://github.com/typekit/webfontloader > https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js
+//https://github.com/typekit/webfontloader#custom
 const preloadFonts = (id) => {
     return new Promise((resolve) => {
         WebFont.load({
             typekit: {
                 id: id
             },
+            google: {
+                families: ['Droid Sans', 'Droid Serif']
+            },
+            custom: {
+                families: ['My Font', 'My Other Font:n4,i4,n7'],
+                //urls: ['/fonts.css']
+            },
             active: resolve
         });
     });
 };
+
+
+//If element available 
+if(!!el.querySelector(".classname")) {
+    // do something
+}
+
+
+//For loop
+for (let i = 0; i < 5; i++) {
+    // do something
+    console.log(i);
+}
+
+
+// Has Class checking
+el.classList.contains("active");
+
+// Next Sibling
+el.nextElementSibling;
+
+//Get Offset
+const getOffset = (el) => {
+    const rect = el.getBoundingClientRect();
+    return {
+        left: rect.left + window.scrollX,
+        top: rect.top + window.scrollY
+    };
+};
+getOffset('.element');
+
+
+// Outer Height With Margin
+const outerHeight = (el) => {
+    const height = el.offsetHeight;
+    const style = getComputedStyle(el);
+
+    return height + parseInt(style.marginTop) + parseInt(style.marginBottom);
+}
+
+// Position
+const getPosition = (el) => {
+    return {
+        top:  el.offsetTop,
+        left: el.offsetLeft
+    };
+}
+
+// Position Relative To Viewport
+el.getBoundingClientRect();
+
+
+// Prepend
+const prepend = (parent, el) => {  
+    parent.insertBefore(el, parent.firstChild);
+}
+
+//Toggle Class  
+const toggleClass = (el, className) => { 
+    el.classList.toggle(className);
+}
